72bafcb10a47c9bb3867329efdbff082f3452ca9
bahiru-kemer <bahiru@kemercode.com>
2023-07-08 10:58:09 +0300

    index file is created

---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+// function
+const Events = () => {
+    // return the component
+  return (
+    // root element of this page
+    <div>
+      <h1>All events here</h1>
+    </div>
+  );
+};
+// export
+export default Events;
---
+++ pages/index.js
@@ -0,0 +1,10 @@
+// function
+const Home = () => {
+  return (
+    <div>
+      <h1>Home page here</h1>
+    </div>
+  );
+};
+// export
+export default Home;
